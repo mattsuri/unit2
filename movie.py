4f79549455d9abd672e7b6ebc3a0a80ef331bed2
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-29 08:33:41 -0500

    Updated from Brython Server: 1/29/2018 8:33:43 AM

--- movie.py
+++ movie.py
@@ -1,3 +1,14 @@
 #Matthew Suriawinata
 #1/29/18
 #movie.py - movie rater
+
+age = int(input("Enter your age: ") 
+
+if age > 17:
+    print("You can watch R movies")
+elif age > 13:
+    print("You can watch PG-13 movies")
+elif age > 10:
+    print("You can watch PG movies")
+else:
+    print("You can watch G movies")
