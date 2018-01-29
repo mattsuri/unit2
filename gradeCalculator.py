d99ebf76d1a83c3412957023be63fc4adcfcbbe5
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-29 08:26:23 -0500

    Updated from Brython Server: 1/29/2018 8:26:26 AM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -2,6 +2,15 @@
 #1/29/18
 # grade Calculator.py
 
-grade = int(input("Enter your grade; ")
+grade = int(input("Enter your grade: "))
 
 if grade > 90:
+    print("You earned an A")
+elif grade > 80:
+    print("You earned an B")
+elif grade > 70:
+    print("You earned an C")
+elif grade > 60:
+    print("You earned an D")
+else:
+    print("You earned an F")
