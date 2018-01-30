5ec21478e3b4c43e4d2029f1def6a02d0e0cc0be
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-30 08:24:20 -0500

    Updated from Brython Server: 1/30/2018 8:24:23 AM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,26 @@
+#Matthew Suriawinata
+#1/30/18
+#insulter.py - insults
 
+from random import randint 
+
+
+num = randint(1, 5)
+
+input("Enter your name: ")
+
+if num==1:
+    print("I once owned a monkey that was smarter than you")
+    
+if num==2:
+    print("I once owned a chimp that was smarter than you")
+    
+if num==3:
+    print("I once owned a dog that was smarter than you")
+    
+if num==4:
+    print("I once owned a parrot that was smarter than you")
+    
+if num==5:
+    print("I once owned a rock that was smarter than you")
+    
