7d44b7581ffbd3cc5c80b1b12b80af5f108e5e9d
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-30 08:14:20 -0500

    Updated from Brython Server: 1/30/2018 8:14:22 AM

--- compoundDemo.py
+++ compoundDemo.py
@@ -1,6 +1,18 @@
 #Matthew Suriawinata
 #1/30/18
-#compoundDemo.py
+#compoundDemo.py - how to use and/or
 
 
+num = float(input("Enter a number: "))
+
+if num > 0 and num%7 == 0:
+    print(num, "is positive and divisable by 7")
+    
+
+elif num > 0 and num%7 != 0:
+    print(num, "is positive and not divisable by 7")
+    
+
+
+    
 
