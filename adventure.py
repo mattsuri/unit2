5763fa8273c3d0f89f26e4cfa5140ffd59ec087e
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-30 08:34:44 -0500

    Updated from Brython Server: 1/30/2018 8:34:47 AM

--- adventure.py
+++ adventure.py
@@ -1,1 +1,30 @@
+#Matthew Suriawinata
+#1/30/18
+#adventure.py- adventures
 
+print("A bear is chasing you")
+
+print("You come to a river")
+
+print("Do you jump in the river or run along the river?")
+
+jumpOrRun = input("Jump or Run? ")
+
+if jumpOrRun == "Jump" :
+    print("Thankfully you know how to swim")
+else:
+    print("You run and the bear catches up and eats you" )
+    
+    
+    
+print("However, you notice a nearby shark")
+
+print("Do you punch the shark or swim? ")
+
+punchOrSwim = input("Punch or Swim: ")
+
+
+if punchOrSwim == "Swim" :
+    print("You sucessfully esacped out of the cave")
+else:
+    print("You punch the shark and it fights back eating you" )
