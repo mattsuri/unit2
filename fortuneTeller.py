f9cea81c9864dd77c13214a5bf042231936befc2
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-30 08:10:03 -0500

    Updated from Brython Server: 1/30/2018 8:10:05 AM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -19,5 +19,18 @@ if color==red and number==3:
     print("You will be attacked by a boar")
     
 if color==red and number==4:
-    print("You will be attacked by a eagle")
+    print("You will be attacked by a frog")
+    
+    
+if color==blue and number==4:
+    print("You will be attacked by a pear")
+    
+if color==blue and number==3:
+    print("You will be attacked by a apple")
+    
+if color==blue and number==2:
+    print("You will be attacked by a beagle")
+    
+if color==blue and number==1:
+    print("You will be attacked by a mango")
 
