077ce62aaa071cd0d3987653d3f8697f4c1f2add
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-30 08:20:17 -0500

    Updated from Brython Server: 1/30/2018 8:20:18 AM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -2,35 +2,36 @@
 #1/30/18
 #fortuneTeller.py - tells you a fortune
 
-color = input("Pick a color red or blue")
+color = input("Pick a color red or blue: ")
 
 number = int(input("Pick a number 1-4: "))
 
 
-if color==red and number==1:
+
+if color=="red" and number==1:
     print("You will be attacked by a bear")
     
     
-if color==red and number==2:
+if color=="red" and number==2:
     print("You will be attacked by an eagle")
 
 
-if color==red and number==3:
+if color=="red" and number==3:
     print("You will be attacked by a boar")
     
-if color==red and number==4:
+if color=="red" and number==4:
     print("You will be attacked by a frog")
     
     
-if color==blue and number==4:
+if color=="blue" and number==4:
     print("You will be attacked by a pear")
     
-if color==blue and number==3:
+if color=="blue" and number==3:
     print("You will be attacked by a apple")
     
-if color==blue and number==2:
+if color=="blue" and number==2:
     print("You will be attacked by a beagle")
     
-if color==blue and number==1:
+if color=="blue" and number==1:
     print("You will be attacked by a mango")
 
