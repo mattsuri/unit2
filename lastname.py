2cdd818e34dcb538f7c7591ff18157083389906e
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-31 08:27:43 -0500

    Updated from Brython Server: 1/31/2018 8:27:42 AM

--- lastname.py
+++ lastname.py
@@ -1,1 +1,11 @@
+#Matthew Suriawinata
+#1/31/18
+#lastname.py - tells whether last name is in the first or second half of the alaphabet
+
+last_name = input("Enter your last name: ")
+lastName = last_name.lower()
+if lastName > "a" and lastName < "m":
+    print("Your name is in the first half of the alphabet")
+else:
+    print("Your name is the the second half of the alphabet")
 
