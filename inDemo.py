26411bdc3bb397876e306369679875685974af7d
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-31 08:07:34 -0500

    Updated from Brython Server: 1/31/2018 8:07:33 AM

--- inDemo.py
+++ inDemo.py
@@ -1,3 +1,10 @@
 #Matthew Suriawinata
 #1/31/18
 #inDemo.py - use conditionals with strings
+
+word = linput("Enter a word: ")
+
+if "w" or "W" in word: 
+    print(word, "is cool")
+else:
+    print(word, "is NOT cool")
