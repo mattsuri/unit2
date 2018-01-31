49d18e49f3ebc47e88e2c17d9fc7fe17ed4ef2ba
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-01-31 08:16:41 -0500

    Updated from Brython Server: 1/31/2018 8:16:40 AM

--- warmup3.py
+++ warmup3.py
@@ -1,1 +1,14 @@
+#Matthew Suriawinata        
+#1/31/18
+#warmup3.py 
 
+num = int(input("Enter a number: "))
+
+if num%2==0 and num%3==0:
+    print(num, "is divisable by 2 and 3")
+elif num%2==0:
+     print(num, "is divisable by 2")
+elif num%3==0:
+     print(num, "is divisable by 3")
+else:
+    print(num, "is not divisable by 2 or 3")
