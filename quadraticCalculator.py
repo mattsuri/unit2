1983b4db5490028241325884e942a758b8e49f9e
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-02-05 08:48:48 -0500

    Updated from Brython Server: 2/5/2018 8:48:49 AM

--- quadraticCalculator.py
+++ quadraticCalculator.py
@@ -1,1 +1,22 @@
+#Matthew Suraiwinata
+#2/5/18
+#quadraticCalculator.py - quadratic calculator 
+
+import sqrt from math
+
+a = float(int("Enter a value for A: "))
+b = float(int("Enter a value for B: "))
+c = float(int("Enter a value for C: "))
+
+
+discriminate = b**2 - (4 * a * c)
+
+postiveAns = (b + discriminate)/ 2*a
+
+negativeAns (b + discriminate)/ 2*a
+
+print(postiveAns)
+print(negativeAns)
+
+
 
